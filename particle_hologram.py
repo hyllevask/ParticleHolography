3715ad8f832d24337193de86a294c30d98197935
Johan Öhman <jhn.ohman@gmail.com>
2023-08-02 22:50:44 +0200

    Started fiddeling with the design.

---
+++ particle_hologram.py
@@ -0,0 +1,17 @@
+class ParticleHologram:
+    def __init__(self):
+        self.input = None
+        self.detector = None
+        self.laser = None
+    def generate input(self.)
+    def forward_operator(self,input):
+        pass
+
+
+class lightsource:
+    def __init__(self):
+        self.name = None
+        self.power = None
+        self.wavelength = None
+    def print(self):
+        print(self.name)
